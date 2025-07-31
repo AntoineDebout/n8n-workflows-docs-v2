1e6d5818722bdd588abdef6cea7a8187d3ecf0f8
Antoine DEBOUT <antoine.debout.95@gmail.com>
2025-08-01 00:53:58 +0200

    Removed Welcome and Dashboard vue

--- resources/js/Components/Workflows/WorkflowCard.vue
+++ resources/js/Components/Workflows/WorkflowCard.vue
@@ -173,6 +173,7 @@ const deleteWorkflow = () => {
 .line-clamp-2 {
   display: -webkit-box;
   -webkit-line-clamp: 2;
+  line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
@@ -180,6 +181,7 @@ const deleteWorkflow = () => {
 .line-clamp-3 {
   display: -webkit-box;
   -webkit-line-clamp: 3;
+  line-clamp: 3;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
