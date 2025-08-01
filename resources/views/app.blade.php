f72c732c3b95f690b13ef34ec980f78a2e3b2c0d
Antoine DEBOUT <antoine.debout.95@gmail.com>
2025-08-01 12:51:47 +0200

    Added n8n Integration

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <script src="https://cdn.jsdelivr.net/npm/@webcomponents/webcomponentsjs@2.0.0/webcomponents-loader.js"></script>
+        <script src="https://www.unpkg.com/lit@2.0.0-rc.2/polyfill-support.js"></script>
+        <script type="module" src="https://cdn.jsdelivr.net/npm/@n8n_io/n8n-demo-component/n8n-demo.bundled.js"></script>
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
